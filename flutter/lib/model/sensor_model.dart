e34ea127af782d889e7cfb1a40eeeab89794f067
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2022-12-13 09:47:04 +0900

    feat: Sensor 모델 추가
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

---
+++ flutter/lib/model/sensor_model.dart
@@ -0,0 +1,11 @@
+class Sensor {
+  final double _x;
+  final double _y;
+  final double _z;
+
+  Sensor(this._x, this._y, this._z);
+
+  double get x => _x;
+  double get y => _y;
+  double get z => _z;
+}
