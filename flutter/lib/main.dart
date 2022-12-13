83b9354f600a156fb49b6cd75146d022f03e9d63
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2022-12-14 00:07:00 +0900

    feat: Material 3 활성화
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         theme: ThemeData(
           primarySwatch: Colors.blue,
+          useMaterial3: true,
         ),
         home: const MainScreen(),
       ),
