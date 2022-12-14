ae2ed9e7077db654c529e11338172f037e220122
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2022-12-14 13:56:07 +0900

    flutter: feat: SpeedDial 추가
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- flutter/lib/screen/log_screen.dart
+++ flutter/lib/screen/log_screen.dart
@@ -1,6 +1,7 @@
 import 'package:emb_motion/provider/socket_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:simple_speed_dial/simple_speed_dial.dart';
 
 class LogScreen extends StatelessWidget {
   LogScreen({super.key});
@@ -29,11 +30,30 @@ class LogScreen extends StatelessWidget {
           }),
           Align(
             alignment: Alignment.bottomRight,
-            child: FloatingActionButton(
-              child: const Icon(Icons.delete_forever_outlined),
-              onPressed: () {
-                _socketProvider.clearLogs();
-              },
+            child: SpeedDial(
+              speedDialChildren: [
+                SpeedDialChild(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.green,
+                  child: const Icon(Icons.settings_outlined),
+                  onPressed: () {},
+                ),
+                SpeedDialChild(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.redAccent,
+                  child: const Icon(Icons.delete_forever_outlined),
+                  onPressed: () {},
+                ),
+                SpeedDialChild(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.lightBlueAccent,
+                  child: const Icon(Icons.play_arrow_outlined),
+                  onPressed: () {
+                    _socketProvider.clearLogs();
+                  },
+                ),
+              ],
+              child: const Icon(Icons.menu_outlined),
             ),
           ),
         ],
